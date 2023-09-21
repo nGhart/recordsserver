28467fe83291742eaf51835f7f97d5fedb6e16a9
nGhart <107068059+nGhart@users.noreply.github.com>
2023-09-21 20:00:27 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 //   require('dotenv').config();
 // }
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const connectToDb = require('./config/connectToDb');
