0da021e3e2b45ad8dbd34453a6ca371eac769110
nGhart <107068059+nGhart@users.noreply.github.com>
2023-09-22 15:50:40 +0000

    Update server.js dep

--- server.js
+++ server.js
@@ -27,12 +27,15 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: true,
+//     credentials: true,
+//   })
+// );
+
+//deploy
+app.use(cors());
 
 //DB connection
 connectToDb();
