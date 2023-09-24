fab3899de1b28df4c527bb6b27b62c87148e9c10
nGhart <107068059+nGhart@users.noreply.github.com>
2023-09-24 18:17:04 +0000

    Update requireAuth.js

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -5,30 +5,8 @@ async function requireAuth(request, response, next) {
   try {
     //read token
     const token = request.cookies.Authorization;
-    // if(token){
-    //   jwt.verify(token,"ASEFTHYKLJBGMYTHZ", async(err,decoded)=>{
-    //     if(err){
-    //       console.log(err)
-    //     }else{
-    //       if (Date.now() > decoded.exp) return response.sendStatus(401);
-    //       //find user
-    //          const user = await User.findById(decoded.sub);
-    //        //if user not found
-    //         if (!user) return console.log("user not found");
-    //         //if user found
-    //           request.user = user;
-    //       console.log(request.user)
-    //       next();
-    //     }
-    //   }); 
-    // }else{
-    //   console.log("token not found")
-    // }
-
-    //changes
     //decode token
-    //change
-    const decoded = jwt.verify(token,"ASEFTHYKLJBGMYTHZ");
+    const decoded = jwt.verify(token, process.env.SECRET);
     //make sure token is not expired
     if (Date.now() > decoded.exp) return response.sendStatus(401);
     //find user
@@ -37,13 +15,12 @@ async function requireAuth(request, response, next) {
     if (!user) return response.sendStatus(401);
     //if user found
     request.user = user;
-    next();
 
-    
+    next();
   } catch (error) {
-    
     console.log('root of the', error);
     return response.sendStatus(401);
   }
+}
 }
 module.exports = requireAuth;
